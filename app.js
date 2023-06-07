914eeb224434aee4ccfbe548db7e103bb26aacfd
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-06-07 13:10:23 +0300

    fixes for tests

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use((req, res, next) => {
 app.use("/", cardRoutes);
 app.use("/", userRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500).json({ message: error.message || "Internal Server Error" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
