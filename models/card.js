c3f33785d01df537c4d11b0f61bb36338f7b2dcb
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-06-08 09:24:48 +0300

    new bugs fixed

--- models/card.js
+++ models/card.js
@@ -3,18 +3,18 @@ const { model, Schema, mongoose } = require("mongoose");
 const cardSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   link: {
     type: String,
-    required: [true, 'Поле "link" должно быть заполнено']
+    required: true,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    // required: true,
   },
   likes: {
     type: [mongoose.Schema.Types.ObjectId],
--- models/user.js
+++ models/user.js
@@ -1,30 +1,22 @@
-const validator = require("validator");
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Поле "name" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    },
-    about: {
-      type: String,
-      required: [true, 'Поле "about" должно быть заполнено'],
-      minlength: [2, 'Минимальная длина поля "about" - 2'],
-      maxlength: [30, 'Максимальная длина поля "about" - 30'],
-    },
-    avatar: {
-      type: String,
-      required: [true, 'Поле "avatar" должно быть заполнено'],
-      validate: {
-        validator: (v) => validator.isURL(v),
-        message: "Некорректный URL",
-      },
-    },
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
-  { versionKey: false }
-);
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = model("User", userSchema);
