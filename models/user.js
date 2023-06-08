5d48645a68962496fd1ce3d9905e726462e11c2f
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-06-08 09:04:25 +0300

    additional bugs fixes

--- models/user.js
+++ models/user.js
@@ -1,29 +1,30 @@
-// eslint-disable-next-line import/no-import-module-exports
-import validator from "validator";
-
+const validator = require("validator");
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "about" - 2'],
-    maxlength: [30, 'Максимальная длина поля "about" - 30'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "Некорректный URL",
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "about" - 2'],
+      maxlength: [30, 'Максимальная длина поля "about" - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный URL",
+      },
     },
   },
-});
+  { versionKey: false }
+);
 
 module.exports = model("User", userSchema);
